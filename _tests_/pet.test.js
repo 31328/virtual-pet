0d406c0f8ea74391652bb4ca64c58124c0faa68a
31328 <g31328r@gmail.com>
2019-09-19 14:51:21 +0100

    step6

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -45,3 +45,21 @@ describe('fitness', () => {
     expect(pet.fitness).toEqual(7);
   });
 });
+
+describe('walk', () => {
+  it('increases fitness by 4', () => {
+    const pet = new Pet('fido');
+    pet.fitness = 4;
+    pet.walk();
+    expect(pet.fitness).toEqual(8);
+  });
+});
+
+describe('walk', () => {
+  it('increases fitness to a maximum of 10', () => {
+    const pet = new Pet('fido');
+    pet.fitness = 7;
+    pet.walk();
+    expect(pet.fitness).toEqual(MAXIMUM_FITNESS);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,4 +1,6 @@
 
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
@@ -12,4 +14,13 @@ Pet.prototype.growUp = function() {
     this.fitness -= 3;
   };
 
+  Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS ) {
+      this.fitness += 4;
+    } else {
+      this.fitness = MAXIMUM_FITNESS;
+    }
+  }
+
 module.exports = Pet;
+
